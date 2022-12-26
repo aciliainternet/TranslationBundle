be61c5ef9141059e3c9e9541022fe9d155958974
Rodrigo Mendez <rodmen@gmail.com>
2022-12-26 14:18:46 -0300

    Support for doctrine/dbal 3.5.2

--- Library/Translation/Loader.php
+++ Library/Translation/Loader.php
@@ -44,15 +44,15 @@ class Loader
             if ($resource ===  null) {
                 $nodesSql = 'SELECT N.node_name, N.node_type, A.attrib_name, A.attrib_original, NULL AS value_translation FROM translation_node N INNER JOIN translation_attribute A ON N.node_id = A.attrib_node';
                 $nodesStmt = $this->doctrine->getManager()->getConnection()->prepare($nodesSql);
-                $nodesStmt->execute();
+                $nodesStmt = $nodesStmt->executeQuery();
 
             } else {
                 $nodesSql = 'SELECT N.node_name, N.node_type, A.attrib_name, A.attrib_original, V.value_translation FROM translation_node N INNER JOIN translation_attribute A ON N.node_id=A.attrib_node LEFT JOIN translation_value V ON A.attrib_id = V.value_attribute AND (V.value_resource = :resource OR V.value_resource IS NULL)';
                 $nodesStmt = $this->doctrine->getManager()->getConnection()->prepare($nodesSql);
-                $nodesStmt->execute(['resource' => $resource]);
+                $nodesStmt = $nodesStmt->executeQuery(['resource' => $resource]);
             }
 
-            $nodes = $nodesStmt->fetchAll();
+            $nodes = $nodesStmt->fetchAllAssociative();
             foreach ($nodes as $node) {
                 $id = $node['node_name'] . '.' . $node['attrib_name'];
                 $value = $node['value_translation'];
